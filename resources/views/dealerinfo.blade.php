96937a7551a4b5d6a49341da3114b749b671c1d6
Shruti Dhekane <srd@23point4.in>
2017-01-30 15:52:28 +0530

    Added search functionality for dealer info and consumer info pages

--- resources/views/dealerinfo.blade.php
+++ resources/views/dealerinfo.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dealer Details</div>
+                <div class="panel-heading">Dealer Details
+					<!-- search box for filtering -->
+					<div class="form-group pull-right">
+						<input type="text" class="search form-control" placeholder="What you looking for?">
+					</div>
+					<span class="counter pull-right"></span>
+				</div>
 
                 <div class="panel-body">
                     					
@@ -42,4 +48,40 @@
         </div>
     </div>
 </div>
+
+<script>
+
+$(document).ready(function() {
+
+$(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });	
+	
+	
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+    
+  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' Orders');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+		  });
+});
+</script>
 @endsection
